1d32ce292c4f26aea50617fbf228285d1a765ee6
AlexanderPurvis <1703361@uad.ac.uk>
2022-01-29 03:24:59 +0000

    Movement Scripts added

---
+++ GameJam2022/Assets/Scripts/FirstPersonCam.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstPersonCam : MonoBehaviour
+{
+    public float camRotationSpeed = 1;
+    public Transform player;
+    float mouseX, mouseY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CamUpdate();
+    }
+
+    void CamUpdate()
+    {
+        mouseX += Input.GetAxis("Mouse X") * camRotationSpeed;
+        mouseY -= Input.GetAxis("Mouse Y") * camRotationSpeed;
+
+        mouseY = Mathf.Clamp(mouseY, -7, 15);
+        transform.LookAt(this.transform);
+        this.transform.rotation = Quaternion.Euler(mouseY, mouseX, 0.0f);
+        player.rotation = Quaternion.Euler(0.0f, mouseX, 0.0f);
+    }
+}
---
+++ GameJam2022/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController controller;
+    public float speed = 6.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+    //    float Horizontal = Input.GetAxisRaw("Horizontal");
+    //    float Vertical = Input.GetAxisRaw("Vertical");
+
+    //    Vector3 direction = new Vector3(Horizontal, 0.0f, Vertical).normalized;
+
+
+    //    if(direction.magnitude >= 0.1f)
+    //    {
+    //        controller.Move(direction * speed * Time.deltaTime);           
+    //    }
+    }
+}
